65adbc2166740ce5b258cc95e0d769877ad89739
Maulana-anjari <maulana17anjari@gmail.com>
2021-12-27 17:40:29 +0700

    add yargs module

--- App.js
+++ App.js
@@ -1,10 +1,2 @@
-const contacts = require('./contacts.js');
-const showQuestion = async () => {
-    const nama = await contacts.tulisPertanyaan("Masukkan nama Anda : ");
-    const email = await contacts.tulisPertanyaan("Masukkan email Anda : ");
-    const nomorHP = await contacts.tulisPertanyaan("Masukkan nomor HP Anda : ");
-    contacts.saveContact(nama, email, nomorHP);
-
-}
-
-showQuestion();
+const yargs = require("yargs")
+console.log(yargs.argv)
